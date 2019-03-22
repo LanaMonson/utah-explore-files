e1b3f53d6551fd7408dd053700924aa8296041fc
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-21 19:57:51 -0600

    Create EatPage.js

---
+++ EatPage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class EatPage extends Component {
+    state = {
+
+    }
+    render() {
+        return(
+            <div >
+                EatPage
+            </div>
+        )
+    }
+}
+
+export default EatPage;
