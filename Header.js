19a7997ad08e8b311465be23a6369402568e96fa
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-21 19:44:57 -0600

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import './Header.css';
+
+//Stateless functional component below
+const Header = () => {
+    return(
+        <div className = "eu-header">
+            <div className = "eu-header-content">
+                <img className = "eu-logo" src="./..." alt=" " />
+            </div>
+        </div>
+    )
+}
+
+export default Header;
