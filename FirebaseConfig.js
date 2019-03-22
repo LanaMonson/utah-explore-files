d197edeaf744b08e4330350f71e476591e2a6be9
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-22 09:36:04 -0600

    Create FirebaseConfig.js

---
+++ FirebaseConfig.js
@@ -0,0 +1,13 @@
+import Firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyAMRWbLLJjss9zxi7y4ZSqJ-GPwJe6zd2k",
+    authDomain: "explore-utah27.firebaseapp.com",
+    databaseURL: "https://explore-utah27.firebaseio.com",
+    projectId: "explore-utah27",
+    storageBucket: "explore-utah27.appspot.com",
+    messagingSenderId: "871693138938"
+  };
+  const firebaseApp = Firebase.initializeApp(config);
+  const db = firebaseApp.database()
+  export const dbMenuRef = db.ref('menu')
