02893ef6b001fffa89408134c8e81d6a26247d7e
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-21 19:44:27 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+//import App from './App';
+import Layout from '/Users/svetlanamonson/projects/utah-explore/utah-explore/src/components/Layout.js';
+import './index.css';
+//import * as serviceWorker from './serviceWorker';
+
+
+ReactDOM.render(<Layout />, document.getElementById('root'));
+//serviceWorker.unregister();
