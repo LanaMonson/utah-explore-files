1043746d8dd8a29f455e873ec7cf5fd17db21214
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-22 09:35:32 -0600

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+//import image 
+//firebase fetching
+import './Home.css';
+
+class Home extends Component {
+    state = {
+
+    }
+    render() {
+        return(
+            <div>
+                <img src="/Users/svetlanamonson/projects/utah-explore/utah-explore/src/components/Home/mesaarch-sunrise.png" alt="mesaarch-sunrise" />
+                
+            </div>
+        )
+    }
+}
+export default Home;
