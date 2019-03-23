2edbfb57ac9a6961ab0f3468e90060f489c2342c
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-22 20:13:35 -0600

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,16 +1,47 @@
 import React, {Component} from 'react';
-//import image 
+//import {API_URL, IMAGE_BASE_URL } <---- IMPORT IMAGE
 //firebase fetching
+
+//import Header from './Header/Header';
+//import NavigationBar from './NavigationBar/NavigationBar';
+//import Footer from './Footer/Footer';
+//import Spinner from './Spinner/Spinner';
 import './Home.css';
 
 class Home extends Component {
     state = {
-
+        image: [],
+        loading: false,
     }
+
+    //lifecycle method
+    componentDidMount() {
+        this.setState({loading: true});
+        //const endpoint = `${API_URL}`
+        //this.fetchItems(endpoint);
+    }
+
+    // fetchItems = (endpoint) => {
+    //     fetch(endpoint)
+    //     .then(result => result.json())
+    //     .then(result => {
+    //         this.setState({
+    //                image: [...this.state.image, ...result.results],
+    //                loading: false,
+    //         })
+    //     })
+    // }
+
     render() {
         return(
             <div>
-                <img src="/Users/svetlanamonson/projects/utah-explore/utah-explore/src/components/Home/mesaarch-sunrise.png" alt="mesaarch-sunrise" />
+                {/* 
+                <Header />
+                <NavigationBar />
+                <Footer />
+                */}
+                <img src="/Users/svetlanamonson/projects/utah-explore/utah-explore/src/components/Home/mesaarch-sunrise.png" 
+                alt="mesaarch-sunrise" />
                 
             </div>
         )
