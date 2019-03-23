e4cfba1ada89696bf83b7e35a7d3071cc33381f0
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-22 20:11:59 -0600

    Create ActivityPage.js

---
+++ ActivityPage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class ActivityPage extends Component {
+    state = {
+
+    }
+    render() {
+        return(
+            <div >
+                TwoColumns
+            </div>
+        )
+    }
+}
+
+export default ActivityPage;
