696e941450a6e3eb34237484b50aa3defae6c877
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-24 21:57:56 -0600

    Update Routing.js

--- Routing.js
+++ Routing.js
@@ -1,10 +1,7 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Home from './components/Home/Home';
-import ActivityPage from './components/ActivityPage/ActivityPage';
-import RelaxPage from './components/RelaxPage/RelaxPage';
-import EatPage from './components/EatPage/EatPage';
-//import * as serviceWorker from './serviceWorker';
+import EatPage from '../EatPage/EatPage';
+//import * as serviceWorker from '../serviceWorker';
+//import * as serviceWorker from '/Users/svetlanamonson/projects/utah-explore/utah-explore/src/serviceWorker.js';
+
 
 
 class Routing extends Component {
