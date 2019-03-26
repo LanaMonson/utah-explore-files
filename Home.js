4b4d38084354c9fbcfd020febdccc1e993516e3c
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-25 20:15:33 -0600

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,36 +1,27 @@
 import React, {Component} from 'react';
+import Carousel from "../Home/Carousel";
 //firebase fetching
 
-import Header from '../Header/Header';
-import NavigationBar from '../NavigationBar/NavigationBar';
-import Footer from '../Footer/Footer';
-//import image from '../images/mesaarch-sunrise.png'  <----?????
+
+import imgUrl from '../Home/camping.jpg'
 
 import './Home.css';
 
 class Home extends Component {
-    state = {
-        image: [],
-        loading: false,
-    }
+    // state = {
+    //     image: [],
+    //     loading: false,
+    // }
 
     render() {
         return(
             <div>
-                {/* 
-                <Header />
-                <NavigationBar />
-                <Footer />
-                */}
-                <img src="../images/mesaarch-sunrise.png" 
-                alt="mesaarch-sunrise" />
+                <Carousel/> 
+                
                 
             </div>
         )
     }
 }
-export default Home;
 
-// IMAGE PATH:
-// /Users/svetlanamonson/projects/utah-explore/utah-explore/src/components/images/mesaarch-sunrise.png
-// "../images/mesaarch-sunrise.png"
+export default Home;
