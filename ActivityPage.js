f05ac64ce918b034f66b0729324253b0747d77c1
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-04-01 18:27:08 -0600

    Update ActivityPage.js

--- ActivityPage.js
+++ ActivityPage.js
@@ -1,16 +1,25 @@
 import React, {Component} from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import './ActivityPage.css';
 
-class ActivityPage extends Component {
-    state = {
-
-    }
-    render() {
-        return(
-            <div >
-                TwoColumns
-            </div>
-        )
-    }
+export default class ActivityPage extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>.column1</Col>
+          <Col>.column2</Col>
+        </Row>
+        <Row>
+          <Col>.column3</Col>
+          <Col>.column4</Col>
+        </Row>
+        <Row>
+          <Col sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
+          <Col sm={{ size: 'auto', offset: 2 }}>.col-sm-auto .offset-sm-2</Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
-export default ActivityPage;
