7bdb3f7751dae1bd0c2ba9a1c09af3623d30a1f6
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-04-02 21:51:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-//import App from './App';
 import Layout from '/Users/svetlanamonson/projects/utah-explore/utah-explore/src/components/Layout.js';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 //import * as serviceWorker from './serviceWorker';
 
 
